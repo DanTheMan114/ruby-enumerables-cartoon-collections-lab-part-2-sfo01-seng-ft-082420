045d9d32dba78f91325cc38d282bbcff52df2a64
Daniel Turner <danielturner184@gmail.com>
2020-08-17 05:02:14 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,16 @@
 def square_array(array)
+  new_array = []
+  index = 0
+ 
+  while index < array.length do
+    new_array << array[index] * array[index]
+    index += 1
+  end
+ 
+  new_array
+end
+ 
+square_array([1,2,3,4,5])
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
 end
